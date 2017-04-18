fa8145cf1a29ce10e73e191c766800e11825fd83
abai168 <1061663605@qq.com>
2017-04-18 19:57:31 +0800

    登录对接

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,32 +2,81 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    PlateNo: ''
-  },
-  //事件处理函数
-  bindPlateNo:function(e){
-    this.setData({
-      PlateNo:e.detail.value
-    })
-  },
-  searchPosition: function() {
-    wx.request({
-      url: '/cmd.svc/Admin/Vehicle/VehicleManager/QuerySingleLastPosition',
-      data:{
-        PlateNo:this.data.PlateNo
-      },
-      header:{
-        'content-type':'application/json'
-      },
-      success:function(res){
-        console.log(res.data)
-      }
-    })
-  },
-  toPhoto:function(){
-    wx.navigateTo({
-      url:'../photo/photo'
-    })
-  }
+    onReady: function() {
+        var token = wx.getStorageSync('Token')
+        wx.request({
+            url: 'http://172.16.19.27:8032/wxmini/User/Auth',
+            method: 'GET',
+            header: {
+                'content-type': 'application/json',
+                'token': token
+            },
+            success: function(res) {
+                if (res.data.code == 1002 || res.data.code == 1003 || res.data.code == 1004 || res.data.code == 1005) {
+                    wx.redirectTo({
+                        url: '../login/login'
+                    })
+                }
+            },
+            faile: function(res) {
+                console.log(res.data);
+                console.log('is failed')
+            }
+        })
+    },
+    data: {
+        PlateNo: '',
+        currentTab: 0,
+        date: '查询日期', //默认日期
+        minDate: '2007-01-01', //开始日期上限
+        maxDte: '2027-12-31' //结束日期下限
+    },
+    //事件处理函数
+    //切换
+    switchNav: function(e) {
+        var that = this;
+        if (this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            that.setData({
+                currentTab: e.target.dataset.current
+            })
+        }
+    },
+    bindChange: function(e) {
+        var that = this;
+        that.setData({
+            currentTab: e.detail.current
+        });
+    },
+    //日期
+    dateChange: function(e) {
+        this.setData({
+            date: e.detail.value
+        })
+    },
+    bindPlateNo: function(e) {
+        this.setData({
+            PlateNo: e.detail.value
+        })
+    },
+    searchPosition: function() {
+        wx.request({
+            url: '/cmd.svc/Admin/Vehicle/VehicleManager/QuerySingleLastPosition',
+            data: {
+                PlateNo: this.data.PlateNo
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res.data)
+            }
+        })
+    },
+    toPhoto: function() {
+        wx.navigateTo({
+            url: '../photo/photo'
+        })
+    }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,53 +1,58 @@
 //login.js
 var app = getApp()
 Page({
-	data:{
-		UserName: '',
-		Password: '',
-		Token:'',
-		response:''
-	},
-	// 事件处理函数
-	bindUserName:function(e){
-		this.setData({
-			UserName:e.detail.value
-		})
-	},
-	bindPassword:function(e){
-		this.setData({
-			Password:e.detail.value
-		})
-	},
-	login:function(){
-		wx.request({
-			url:'https://172.16.17.181/cmd.svc/WeixinMini/User/UserManager/Login',
-			data:{
-				UserName: this.data.UserName,
-				Password: this.data.Password
-			},
-			method:'POST',
-			header:{
-				'content-type':'application/json'
-			},
-			success:function(res){
-				this.setData({
-					Token:res.data.tokenKey,
-					response:res
-				})
-				try{
-					wx.serStorageSync('Token',res.data.tokenKey)
-				}catch(e){
+    data: {
+        UserName: '',
+        Password: '',
+        Token: '',
+        response: ''
+    },
+    // 事件处理函数
+    bindUserName: function(e) {
+        this.setData({
+            UserName: e.detail.value
+        })
+    },
+    bindPassword: function(e) {
+        this.setData({
+            Password: e.detail.value
+        })
+    },
+    login: function() {
+        var that = this
+        wx.request({
+            url: 'http://172.16.19.27:8032/wxmini/User/Login',
+            data: {
+                UserName: this.data.UserName,
+                Password: this.data.Password
+            },
+            method: 'POST',
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                if (res.data.code == 1001) {
+                    that.setData({
+                        //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJJZCI6IjM0YmU0ODAwLTIxMDMtNDQ0Yy1iMTBmLWE2NGEwYjBkMzY4YyIsIktleSI6bnVsbCwiRGF0ZSI6IlwvRGF0ZSgxNDkzNzk1MjYwMTk5KVwvIn0.Cuc4rXm1OpgeJBN3J14qWYEIGSVXI-xXqflrNXps0ck
+                        Token: res.data.result,
+                        response: res
+                    })
+                    try {
+                        wx.setStorageSync('Token', res.data.result)
+                    } catch (e) {
 
-				}
-				wx.navigateTo({
-					url:'../index/index'
-				})
-				console.log(res.data);
-			},
-			faile:function(res){
-				console.log(res.data);
-				console.log('is failed')
-			}
-		})
-	}
+                    }
+                    wx.navigateTo({
+                        url: '../index/index'
+                    })
+                    console.log(res.data);
+                }
+
+            },
+            faile: function(res) {
+                console.log(res.data);
+                console.log('is failed')
+            }
+        })
+    }
 })
