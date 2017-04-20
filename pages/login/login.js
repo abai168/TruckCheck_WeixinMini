ded1d2f9f1b716b5ce147b7e90091311be7280e7
abai168 <1061663605@qq.com>
2017-04-20 10:22:00 +0800

    code枚举 封装请求 登录修改，调用封装的请求

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,6 @@
 //login.js
+// 导入js
+var rest = require('../../utils/rest.js')
 var app = getApp()
 Page({
     data: {
@@ -20,36 +22,30 @@ Page({
     },
     login: function() {
         var that = this
-        wx.request({
-            url: 'http://172.16.19.27:8032/wxmini/User/Login',
-            data: {
+        rest.POST({
+            api: { ctrl: 'User', action: 'Login' },
+            params: {
                 UserName: this.data.UserName,
                 Password: this.data.Password
             },
-            method: 'POST',
-            header: {
-                'content-type': 'application/json'
-            },
             success: function(res) {
-                if (res.data.code == 1001) {
-                    that.setData({
-                        //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJJZCI6IjM0YmU0ODAwLTIxMDMtNDQ0Yy1iMTBmLWE2NGEwYjBkMzY4YyIsIktleSI6bnVsbCwiRGF0ZSI6IlwvRGF0ZSgxNDkzNzk1MjYwMTk5KVwvIn0.Cuc4rXm1OpgeJBN3J14qWYEIGSVXI-xXqflrNXps0ck
-                        Token: res.data.result,
-                        response: res
-                    })
-                    try {
-                        wx.setStorageSync('Token', res.data.result)
-                    } catch (e) {
+                that.setData({
+                    //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJJZCI6IjM0YmU0ODAwLTIxMDMtNDQ0Yy1iMTBmLWE2NGEwYjBkMzY4YyIsIktleSI6bnVsbCwiRGF0ZSI6IlwvRGF0ZSgxNDkzNzk1MjYwMTk5KVwvIn0.Cuc4rXm1OpgeJBN3J14qWYEIGSVXI-xXqflrNXps0ck
+                    Token: res.data.result,
+                    response: res
+                })
+                try {
+                    wx.setStorageSync('Token', res.data.result)
+                } catch (e) {
 
-                    }
-                    wx.navigateTo({
-                        url: '../index/index'
-                    })
-                    console.log(res.data);
                 }
+                wx.navigateTo({
+                    url: '../index/index'
+                })
+                console.log(res.data);
 
             },
-            faile: function(res) {
+            fail: function() {
                 console.log(res.data);
                 console.log('is failed')
             }
---
+++ utils/rest.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+var enums = require('enums.js')
+
+var BASE_URL = "http://172.16.19.27:8032/wxmini"
+
+var requestHandler = {
+    api: {},
+    params: {},
+    success: function() {},
+    fail: function() {}
+}
+
+// GET请求
+function GET(requestHandler) {
+    request('GET', requestHandler)
+}
+
+// POST请求
+function POST(requestHandler) {
+    request('POST', requestHandler)
+}
+
+function request(method, requestHandler) {
+    var API_URL = BASE_URL + '/' + requestHandler.api.ctrl + '/' + requestHandler.api.action;
+    wx.request({
+        url: API_URL,
+        data: requestHandler.params,
+        method: method,
+        // header:{},
+        success: function(res) {
+            resultByCode(res.data.code)
+        },
+        fail: function() {
+            requestHandler.fail()
+        },
+        complete: function() {
+
+        }
+    })
+}
+
+function resultByCode(code) {
+    switch (code) {
+        case 1001:
+            requestHandler.success(res);
+            break;
+        case 1002:
+        case 1003:
+        case 1004:
+        case 1005:
+            wx.redirectTo({
+                url: '../login/login'
+            })
+            break;
+        case 1006:
+        case 2001:
+        case 9001:
+        case 9002:
+        case 9003:
+            wx.showModal({
+                title: '提示',
+                content: enums.resultCode[code],
+                success: function(res) {
+                    if (res.confirm) {
+                        console.log('用户点击确定')
+                    } else if (res.cancel) {
+                        console.log('用户点击取消')
+                    }
+                }
+            })
+            break;
+    }
+}
+
+// 通过module.exports暴露对象，通过require来获取对象
+module.exports = {
+    GET: GET,
+    POST: POST
+}
