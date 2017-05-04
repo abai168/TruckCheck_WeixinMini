a1537be7befe2a54e1c88107a7fc6efac342414d
abai168 <1061663605@qq.com>
2017-05-04 15:19:41 +0800

    轨迹查询

--- pages/queryTrajectory/queryTrajectory.js
+++ pages/queryTrajectory/queryTrajectory.js
@@ -3,51 +3,50 @@
  */
 var app = getApp()
 Page({
-    // data: {},
-    // onLoad: function() {
-    //     var trajectory = wx.getStorageSync('Trajectory')
-    data: {},
+    data: {
+        latitude: '',
+        longitude: '',
+        markers: [{
+                id: 1,
+                latitude: '',
+                longitude: '',
+                title: '起点',
+                width: 15,
+                height: 15
+            },
+            {
+                id: 2,
+                latitude: '',
+                longitude: '',
+                title: '终点',
+                width: 15,
+                height: 15
+            }
+        ],
+        polyline: [{
+            points: [],
+            color: "#e207c7",
+            width: 5,
+            dottedLine: false
+        }]
+    },
     onLoad: function() {
+        var temp = wx.getStorageSync('Trajectory')
+            // var temp = [{
+            //     longitude: 113.3245211,
+            //     latitude: 23.10229
+            // }, {
+            //     longitude: 113.324520,
+            //     latitude: 23.21229
+            // }]
         this.setData({
-            markers: [{
-                iconPath: "/resources/others.png",
-                id: 0,
-                latitude: 23.10229,
-                longitude: 113.3245211,
-                width: 50,
-                height: 50
-            }],
-            circles: [{
-                latitude: 23.10229,
-                longitude: 113.3245211,
-                color: '#FF0000DD',
-                fillColor: '#7cb5ec88',
-                radius: 3000,
-                strokeWidth: 1
-            }],
-            polyline: [{
-                    points: [{
-                        longitude: 113.3245211,
-                        latitude: 23.10229
-                    }, {
-                        longitude: 113.324520,
-                        latitude: 23.21229
-                    }],
-                    color: "#e207c7",
-                    width: 30,
-                    dottedLine: false
-                }]
-                // controls: [{
-                //     id: 1,
-                //     iconPath: '/resources/location.png',
-                //     position: {
-                //         left: 0,
-                //         top: 300 - 50,
-                //         width: 50,
-                //         height: 50
-                //     },
-                //     clickable: true
-                // }]
+            latitude: temp[0].latitude,
+            longitude: temp[0].longitude,
+            'markers[0].latitude': temp[0].latitude,
+            'markers[0].longitude': temp[0].longitude,
+            'markers[1].latitude': temp[temp.length - 1].latitude,
+            'markers[1].longitude': temp[temp.length - 1].longitude,
+            'polyline[0].points': temp
         })
     },
     regionchange(e) {
