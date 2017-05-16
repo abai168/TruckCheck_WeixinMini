1ea6de27bdd6128b1dc53a1f3a7f33ad6b8886f1
abai168 <1061663605@qq.com>
2017-05-16 10:44:28 +0800

    a

--- pages/seachCarsDetail/seachCarsDetail.js
+++ pages/seachCarsDetail/seachCarsDetail.js
@@ -98,90 +98,90 @@ Page({
     },
     // 搜索
     searchCars: function() {
-        var that = this
-            // 调用腾讯地图-地址解析-接口,实现地址转坐标
-            // 实例化API核心类
-        var qqmapsdk = new QQMapWX({
-            key: '56KBZ-ICNWP-HNLDI-VWWNG-6NNKE-BPBLZ'
-        });
-        qqmapsdk.geocoder({
-            address: that.data.address,
-            success: function(res) {
-                var lat = res.result.location.lat
-                var lng = res.result.location.lng
-                that.setData({
-                    latitude: lat,
-                    longitude: lng,
-                    markers: [{
-                        id: 0,
+            var that = this
+                // 调用腾讯地图-地址解析-接口,实现地址转坐标
+                // 实例化API核心类
+            var qqmapsdk = new QQMapWX({
+                key: '56KBZ-ICNWP-HNLDI-VWWNG-6NNKE-BPBLZ'
+            });
+            qqmapsdk.geocoder({
+                address: that.data.address,
+                success: function(res) {
+                    var lat = res.result.location.lat
+                    var lng = res.result.location.lng
+                    that.setData({
                         latitude: lat,
                         longitude: lng,
-                        width: 15,
-                        height: 15,
-                        // iconPath: "../../images/blank.gif",
-                        // title: that.data.address + that.data.radius + "公里范围内车辆数：" + that.data.carnumber + '辆'
-                    }],
-                    circles: [{
-                        latitude: lat,
-                        longitude: lng,
-                        color: '#FF0000DD',
-                        fillColor: '#7cb5ec88',
-                        radius: that.data.radius * 1000, //单位是m
-                        strokeWidth: 1
-                    }]
-                })
-                rest.POST({
-                    api: { ctrl: 'Vehicle', action: 'FindVehicleByPlateNo' },
-                    params: {
-                        Lon: lng,
-                        Lat: lat,
-                        Dist: that.data.radius //单位是km
-                    },
-                    success: function(res) {
-                        var markers = []
-                        that.setData({
-                            carnumber: res.result.length
-                        })
-                        markers.push(that.data.markers[0])
-                        for (var i = 0; i < res.result.length; i++) {
-                            markers.push({
-                                id: i + 1,
-                                latitude: res.result[i].Lat,
-                                longitude: res.result[i].Lon,
-                                width: 15,
-                                height: 15,
-                                title: res.result[i].PlateNo
-                            });
+                        markers: [{
+                            id: 0,
+                            latitude: lat,
+                            longitude: lng,
+                            width: 15,
+                            height: 15,
+                            // iconPath: "../../images/blank.gif",
+                            // title: that.data.address + that.data.radius + "公里范围内车辆数：" + that.data.carnumber + '辆'
+                        }],
+                        circles: [{
+                            latitude: lat,
+                            longitude: lng,
+                            color: '#FF0000DD',
+                            fillColor: '#7cb5ec88',
+                            radius: that.data.radius * 1000, //单位是m
+                            strokeWidth: 1
+                        }]
+                    })
+                    rest.POST({
+                        api: { ctrl: 'Vehicle', action: 'FindVehicleByPlateNo' },
+                        params: {
+                            Lon: lng,
+                            Lat: lat,
+                            Dist: that.data.radius //单位是km
+                        },
+                        success: function(res) {
+                            var markers = []
+                            that.setData({
+                                carnumber: res.result.length
+                            })
+                            markers.push(that.data.markers[0])
+                            for (var i = 0; i < res.result.length; i++) {
+                                markers.push({
+                                    id: i + 1,
+                                    latitude: res.result[i].Lat,
+                                    longitude: res.result[i].Lon,
+                                    width: 15,
+                                    height: 15,
+                                    title: res.result[i].PlateNo
+                                });
+                            }
+                            that.setData({
+                                markers: markers,
+                                message: that.data.address + that.data.radius + "公里范围内车辆数：" + that.data.carnumber + '辆'
+                            })
                         }
-                        that.setData({
-                            markers: markers,
-                            message: that.data.address + that.data.radius + "公里范围内车辆数：" + that.data.carnumber + '辆'
-                        })
-                    }
-                })
-            },
-            fail: function(res) {
-                wx.showModal({
-                    title: '提示',
-                    content: res.message,
-                    success: function(res) {
-                        that.setData({
-                            latitude: '',
-                            longitude: '',
-                            markers: [],
-                            circles: [],
-                            message: '地址中请包含城市名称'
-                        })
-                    }
-                })
-            },
-            complete: function(res) {
-                console.log(res);
-            }
-        })
-    },
-    regionchange(e) {
-        console.log("regionchange===" + e.type)
-    }
+                    })
+                },
+                fail: function(res) {
+                    wx.showModal({
+                        title: '提示',
+                        content: res.message,
+                        success: function(res) {
+                            that.setData({
+                                latitude: '',
+                                longitude: '',
+                                markers: [],
+                                circles: [],
+                                message: '地址中请包含城市名称'
+                            })
+                        }
+                    })
+                },
+                complete: function(res) {
+                    console.log(res);
+                }
+            })
+        }
+        // regionchange(e) {
+        //     console.log("regionchange===" + e.type)
+        // }
 
 })
